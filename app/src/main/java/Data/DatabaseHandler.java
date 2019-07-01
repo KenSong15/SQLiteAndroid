68f8d45aaa316215dfc7deccdc5cc9fc6e3d1f9f
Ken <kensong96@gmail.com>
2019-07-01 17:17:27 -0400

    123D, without debug

--- app/src/main/java/Data/DatabaseHandler.java
+++ app/src/main/java/Data/DatabaseHandler.java
@@ -1,10 +1,13 @@
 package Data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
+import Model.Contact;
 import Utils.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -34,4 +37,40 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         //create table again
         onCreate(sqLiteDatabase);
     }
+
+    //CRUD operation: create read update delete
+    public void addContact(Contact contact){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //making the content
+        ContentValues value = new ContentValues();
+        value.put(Util.KEY_NAME, contact.getName());
+        value.put(Util.KEY_PHONE_NAMBER, contact.getPhoneNumber());
+
+        //insert to row
+        db.insert(Util.TABLE_NAME, null, value);
+        db.close();
+    }
+
+    public Contact getContact(int id){
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Util.TABLE_NAME,
+                new String[] { Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NAMBER},
+                Util.KEY_ID+"=?",
+                new String[] {String.valueOf(id)},
+                null,null,null,null);
+
+        if(cursor != null){
+            cursor.moveToFirst();
+        }
+
+        Contact resContact = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+
+        return resContact;
+
+    }
+
 }
