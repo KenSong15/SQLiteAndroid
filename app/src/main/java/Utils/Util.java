19f931b483ebfe087e35ed8aea69b7e721e3c09c
Ken <kensong96@gmail.com>
2019-07-01 16:24:24 -0400

    121D, finished basic setup

---
+++ app/src/main/java/Utils/Util.java
@@ -0,0 +1,15 @@
+package Utils;
+
+public class Util {
+
+    //Database version
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contactDB";
+    private static final String TABLE_NAME = "contacts";
+
+    //contacts table columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PHONE_NAMBER = "phone_number";
+
+}
